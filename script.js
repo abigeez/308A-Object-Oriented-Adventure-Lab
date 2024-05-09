b9d027f333140c7f56ae9b8ad969a302c5ed2f9f
Abi Garner <abigail.gee@hotmail.com>
2024-05-09 01:18:32 -0400

    copied from lab assignment for example and modified and created own properties and methods within class

--- script.js
+++ script.js
@@ -28,16 +28,39 @@ adventurer.roll();
 const name1 = new Character();
 
 class Character {
-  constructor(name,) {
+  constructor(name,health,inventory) {
     this.name = name;
     this.health = 100;
     this.inventory = [];
   }
-  roll () {
-    
+  roll (mod = 100) {
+const result = math.floor(math.random()* 200) + 1 + mod;
+console.log(`${this.name} rolled a ${result}.`);
   }
 }
 
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
+////// part 3///////
+class Adventurer extends Character {
+  constructor (name, role) {
+    super(name);
+    // Adventurers have specialized roles.
+    this.role = role;
+    // Every adventurer starts with a bed and 50 gold coins.
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+  // Adventurers have the ability to scout ahead of them.
+  scout () {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+}
 
 
